f986c131dc3fe99e7f3e2b24b68b13e02b0a8f64
SashankMeka1 <63683831+SashankMeka1@users.noreply.github.com>
2021-06-09 23:19:49 -0700

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 graph::graph(){
 	for(int i = 0; i < 20; i++){
-		for(int j = 0; j < 20;j++){
+		for(int j = 0; j < 20;j++){//clear adjacency matrix
 			adj_mat[j][i] =0;
 		}
 	}
 }
-void graph::add_vertex(char * label){
+void graph::add_vertex(char * label){//add vertex
 	if(capacity < 20){
 		strcpy(label_ls[capacity],label);
 		cout << label_ls[capacity];
@@ -17,7 +17,7 @@ void graph::add_vertex(char * label){
 		
 	}
 }
-void graph::add_edge(char *label_one,char*label_two){
+void graph::add_edge(char *label_one,char*label_two){//add edge both vertices as directed
 	cout << label_one;
 	cout << label_two;
 	int idx_one = 21;
@@ -37,7 +37,7 @@ void graph::add_edge(char *label_one,char*label_two){
 	adj_mat[idx_one][idx_two]=adj_mat[idx_two][idx_one] = 1;
 }
 void graph::remove_vertex(char * label){
-	int idx = 21;
+	int idx = 21;//set all connections to 0 remove from label list
 	bool found = 0;
 	for(int i = 0;i < capacity;i++){
 		if(!strcmp(label,label_ls[i])){
@@ -54,7 +54,7 @@ void graph::remove_vertex(char * label){
 	capacity--;
 }
 void graph::remove_edge(char *label_one,char *label_two){
-	int idx_one;
+	int idx_one;//set connection to 0 both ways
 	int idx_two;
 	for(int i =0; i < capacity; i++){
 		if(!strcmp(label_ls[i],label_one)){
@@ -66,7 +66,7 @@ void graph::remove_edge(char *label_one,char *label_two){
 	}
 	adj_mat[idx_one][idx_two]=adj_mat[idx_two][idx_one] = 0;
 }
-void graph::min_distance(int idx){
+void graph::min_distance(int idx){//recursive for each edge check if next edge leads to modify spset
 	for(int i = 0; i < 20; i++){
 		if(adj_mat[idx][i] and i!=idx){
 			if(sptset[i]>sptset[idx] + adj_mat[idx][i]){
@@ -76,7 +76,7 @@ void graph::min_distance(int idx){
 		}
 	}
 }
-void graph::print(){
+void graph::print(){//print adjacency matrix(for debug)
 	for(int i = 0; i < capacity;i++){
 		for(int j = 0; j<capacity;j++){
 			cout << adj_mat[i][j]<<" ";
@@ -87,7 +87,7 @@ void graph::print(){
 void graph::dijkstra(char * label_one, char*label_two){
 	int idx_one =21;
 	int idx_two = 21;
-	cout << label_ls[0];
+	cout << label_ls[0];//dijkstra check immedaite connections from source and recursive on them and modify sptset
 	cout << label_ls[1];
 	cout << label_ls[2]<<endl;
 	for(int i = 0;i < capacity; i++){
